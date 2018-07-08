598b6a20ef8f91bdec6386c3449964d5c1579d8b
sabyasachi <sabya.nandy14@gmail.com>
2018-07-08 18:03:31 +0530

    Implemented Count Down Timers and Runnable
    Next: App based on count down timers

---
+++ app/src/main/java/com/example/neel/andriod_n_learning/TimerApp.java
@@ -0,0 +1,13 @@
+package com.example.neel.andriod_n_learning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimerApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_app);
+    }
+}
